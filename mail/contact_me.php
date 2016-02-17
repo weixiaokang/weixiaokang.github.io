6824cdba5e7beec7b1e23d32caa8384f88cb79ea
weixiaokang <weixiaokang@freething.cn>
2016-02-17 16:52:38 +0800

    contact_method

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -15,12 +15,18 @@ $email_address = $_POST['email'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
 	
-// Create the email and send the message
-$to = 'root@weixiaokang.cn'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";	
-mail($to,$email_subject,$email_body,$headers);
+$post_data = array('name' => $name, 'email' => $email_address, 'phone' => $phone, 'message' => $message);
+$postdata = http_build_query($post_data);
+$options = array(  
+    'http' => array(  
+      'method' => 'POST',  
+      'header' => 'Content-type:application/x-www-form-urlencoded',  
+      'content' => $postdata,  
+      'timeout' => 15 * 60 // 超时时间（单位:s）  
+    )  
+  );
+$context = stream_context_create($options);
+$result = file_get_contents("http://115.159.115.221/github_blog/contact.php", FALSE, $context); 
+echo $result;
 return true;			
 ?>
